717bc610c8f7da7a3b0307620f6d415245ff1613
AlekseyXD <100358191+AlekseyXD@users.noreply.github.com>
2022-11-01 18:03:49 +0300

    Add files via upload

---
+++ Сalculator/Сalculator/ClassReferences.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сalculator
+{
+    public class ClassReferences
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+        public double Div(double a, double b)
+        {
+            return a / b;
+        }
+        public double Mult(double a, double b)
+        {
+            return a * b;
+        }
+        public double Sin(double s)
+        {
+            return (Math.Sin(s));
+        }
+        public double Cos(double s)
+        {
+            return (Math.Cos(s));
+        }
+        public double Sqrt( double s)
+        {
+            return Math.Sqrt(s);
+        }
+        public double Pov(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+
+    }
+}
---
+++ Сalculator/Сalculator/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Сalculator;
+double a,b,c;
+char oper;
+Console.WriteLine("Добро пожаловать в программу 'Калькулятор'.");
+var res = new ClassReferences();
+char again = 'y';
+while (again == 'y')
+{    
+    Console.WriteLine("Введите оператор: ( + , - , * , / , k(Корень) , ^ ,s(sin) , c(cos) )");
+    oper = Convert.ToChar(Console.ReadLine());
+
+    if (oper == '+')
+    {
+        Console.WriteLine("Введите числа: ");
+        Console.WriteLine();
+        Console.WriteLine("Введите первое число: ");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите второе число: ");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Результат: {res.Add(a, b)}");
+    }
+    else if (oper == '-')
+    {
+        Console.WriteLine();
+        Console.WriteLine("Введите числа: ");
+        Console.WriteLine();
+        Console.WriteLine("Введите первое число: ");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите второе число: ");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Результат: {res.Sub(a, b)}");
+    }
+    else if (oper == '/')
+    {
+        Console.WriteLine();
+        Console.WriteLine("Введите числа: ");
+        Console.WriteLine();
+        Console.WriteLine("Введите первое число: ");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите второе число: ");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Результат: {res.Div(a, b)}");
+    }
+    else if (oper == '*')
+    {
+        Console.WriteLine();
+        Console.WriteLine("Введите числа: ");
+        Console.WriteLine();
+        Console.WriteLine("Введите первое число: ");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите второе число: ");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Результат: {res.Mult(a, b)}");
+    }
+    else if (oper == 'c')
+    {
+        Console.WriteLine("Введите число: ");
+        c = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Результат: {res.Cos(c)}");
+    }
+    else if (oper == 's')
+    {
+        Console.WriteLine("Введите число: ");
+        c = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Результат: {res.Sin(c)}");
+    }
+    else if (oper == '^')
+    {
+        Console.WriteLine();
+        Console.WriteLine("Введите числа: ");
+        Console.WriteLine();
+        Console.WriteLine("Введите первое число: ");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите второе число: ");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Результат: {res.Pov(a, b)}");
+    }
+    else if (oper == 'k')
+    {
+        Console.WriteLine("Введите число: ");
+        c = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Результат: {res.Sqrt(c)}");
+    }
+    else
+    {
+        Console.WriteLine("Неизвестный оператор!!!");
+    }
+
+    Console.WriteLine("Вы хотите продолжить работу с калькулятором? (y/n)");
+    again = Convert.ToChar(Console.ReadLine());
+}
